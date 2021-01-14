c70551297a434a66c6995af5d7825c50de755260
Harish Lal <harishlal80@gmail.com>
2021-01-15 03:51:24 +0530

    Added Opt-Form Component

--- netflix/src/containers/faqs.js
+++ netflix/src/containers/faqs.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Accordion } from '../components';
-import faqsData from '../fixtures/faqs.json';
+import { Accordion, OptForm } from '../components';
+import faqsData from '../fixtures/faqs';
 
 export default function FaqsContainer() {
     return(
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+            <Accordion.Frame>
             {faqsData.map((item) => (
                 <Accordion.Item key={item.id}>
                     <Accordion.Header>{item.header}</Accordion.Header>
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
+            </Accordion.Frame>
+
+            <OptForm>
+                <OptForm.Input placeholder="Email Address"/>
+                <OptForm.Button>GET STARTED</OptForm.Button>
+                <OptForm.Break />
+                <OptForm.Text> Ready To Watch? Enter Your Email To Create Or Restart Your Membership</OptForm.Text>
+            </OptForm>
         </Accordion>    
         )
 }
