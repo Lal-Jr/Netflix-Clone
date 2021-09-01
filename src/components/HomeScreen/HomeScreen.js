e040bbed3514eb5917dd2d546720a909e5206d44
Lal-Jr <harishlal80@gmail.com>
2021-09-01 07:45:26 +0530

    css to scss

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Banner from '../Banner/Banner';
 import Navbar from '../Navbar/Navbar';
-import './HomeScreen.css'
+import './HomeScreen.scss'
 
 const HomeScreen = () => {
     return (
         <div className="homeScreen">
             <Navbar />
+            <Banner />
         </div>
     )
 }
