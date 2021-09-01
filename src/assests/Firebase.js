4243e144e99981f0a64cf96ba2b8b73f5417a5e7
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-09-01 09:24:41 +0530

    scss error fixed, favicon changed and firebase credentials updated

--- src/assests/Firebase.js
+++ src/assests/Firebase.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
+	apiKey: "AIzaSyDce9R70pZeGYJqUB_gJBz0K7HHRSn3tjc",
+	authDomain: "netflix-clone-f8ec1.firebaseapp.com",
+	projectId: "netflix-clone-f8ec1",
+	storageBucket: "netflix-clone-f8ec1.appspot.com",
+	messagingSenderId: "22435360818",
+	appId: "1:22435360818:web:56155abac2dd820597cc2b",
+	measurementId: "G-YJD7E1WQ58",
 };
--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,50 +1,15 @@
-import React from 'react'
-import Banner from '../Banner/Banner';
-import Navbar from '../Navbar/Navbar';
-import './HomeScreen.scss'
+import React from "react";
+import Banner from "../Banner/Banner";
+import Navbar from "../Navbar/Navbar";
+import "./HomeScreen.scss";
 
 const HomeScreen = () => {
-    return (
-        <div className="homeScreen">
-            <Navbar />
-            <Banner />
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-            <h1>test</h1>
-        </div>
-    )
-}
+	return (
+		<div className="homeScreen">
+			<Navbar />
+			<Banner />
+		</div>
+	);
+};
 
 export default HomeScreen;
